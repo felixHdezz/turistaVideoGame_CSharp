04f6b46d233e83b5bf1685b046320336e3834dbe
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2019-05-16 00:21:54 -0500

    Update Var.cs

--- Turista/Turista/Var.cs
+++ Turista/Turista/Var.cs
@@ -7,9 +7,9 @@ namespace Turista
 {
     class Var
     {
-        // Variables globales que se utliza en todo el juego
+        // Variables globales
         
-        // Array para el tablero de lso estados de la republica 
+        // Array para el tablero de los estados de la republica 
         public static int[,] tablero;
         
         // Array para la posiciones iniciales de las imagenes
@@ -21,7 +21,11 @@ namespace Turista
         
         // Estados vendidos
         public static string[,] EstadosVendidos = new string[35, 4];
+        
+        //Hoteles colacados en cada estado de la republica
         public static string[] ColacadosHoteles = new string[35];
+        
+        //Posiciones de los estados de la republica
         public static int[,] Posiciones;
     }
 }
